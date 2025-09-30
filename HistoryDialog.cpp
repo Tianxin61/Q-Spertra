e2c29b13a578c24fe6240c674332365b75aa84af
田心61 <2737504027@qq.com>
2025-09-30 16:54:10 +0800

    Create HistoryDialog.cpp

---
+++ HistoryDialog.cpp
@@ -0,0 +1,39 @@
+#include "HistoryDialog.h"
+#include "ui_HistoryDialog.h"
+#include"DBFactory.h"
+#include"UserInfo.h"
+#include<vector>
+
+using namespace std;
+HistoryDialog::HistoryDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::HistoryDialog)
+{
+    ui->setupUi(this);
+}
+
+HistoryDialog::~HistoryDialog()
+{
+    delete ui;
+}
+
+void HistoryDialog::on_pushButton_clear_clicked()
+{
+    HistoryEntity he;
+    he.userid=UserInfo::getInstance()->getUserid();
+    DBFactory::getInstance()->getHistoryDao()->clearHistory(he);
+}
+void HistoryDialog::on_pushButton_select_clicked()
+{
+    HistoryEntity he;
+    he.userid=UserInfo::getInstance()->getUserid();
+    vector<HistoryEntity> vec;
+    DBFactory::getInstance()->getHistoryDao()->findHistory(he,vec);
+
+    ui->listWidget_history->clear();
+    for(auto item : vec)
+    {
+        QString show=item.name + " " + item.dateTime.toString();
+        ui->listWidget_history->addItem(show);
+    }
+}
