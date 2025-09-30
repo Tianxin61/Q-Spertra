1a7026fd88e4d5ad7ad6b540f31027dbd52ca172
田心61 <2737504027@qq.com>
2025-09-30 16:55:45 +0800

    Create UserInfo.cpp

---
+++ UserInfo.cpp
@@ -0,0 +1,32 @@
+#include "UserInfo.h"
+
+UserInfo* UserInfo::instance=nullptr;
+
+UserInfo *UserInfo::getInstance()
+{
+    if(instance==nullptr)
+    {
+        instance=new UserInfo();
+    }
+    return instance;
+}
+
+void UserInfo::releaseInstance()
+{
+    if(instance!=nullptr)
+    {
+        delete instance;
+        instance=nullptr;
+    }
+}
+
+
+UserInfo::UserInfo()
+{
+
+}
+
+UserInfo::~UserInfo()
+{
+
+}
